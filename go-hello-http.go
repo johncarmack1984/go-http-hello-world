6c0344f4cffa000e93a276f8b60c0b9aef1ced36
John Carmack <johncarmack@me.com>
2024-03-18 15:50:05 -0700

    Add dynamic port selection for HTTP server

--- go-hello-http.go
+++ go-hello-http.go
@@ -3,12 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":80"
+	} else {
+		port = ":" + port
+	}
+
+	return port
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
 
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(getPort(), nil)
 }
